acbdfe5420d5c955a0d82b26d612506e8ef9e395
Lauren Marcus & Heather Anderson <student@epicodus.com>
2015-01-26 16:46:52 -0800

    Allow user to see list of employees for a particular division.

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ also_reload("lib/**/*.rb")
 require("./lib/employee")
 require("./lib/division")
 require("pg")
+require("pry")
 
 
 get('/') do
@@ -24,10 +25,12 @@ end
 
 post('/employees') do
   first_and_last_name = params.fetch('first_and_last_name')
-  @employee = Employee.new({:first_and_last_name => first_and_last_name})
+  division_id = params.fetch("division_id")
+  @employee = Employee.new({:first_and_last_name => first_and_last_name, :division_id => division_id})
   @employee.save()
   erb(:division)
 end
+
 
 get("/employees/:id/edit") do
   @employee = Employee.find(params.fetch("id").to_i())
@@ -44,6 +47,10 @@ end
 
 get("/divisions/:id/edit") do
   @division = Division.find(params.fetch("id").to_i())
+  division_name = @division.division_name()
+  @division.update({:division_name => division_name})
+  @divisions = Division.all()
+  @employees = Employee.all()
   erb(:division_edit)
 end
 
@@ -52,6 +59,6 @@ get("/divisions/:id") do
   division_name = @division.division_name()
   @division.update({:division_name => division_name})
   @divisions = Division.all()
-  @employees = Employee.all()  
+  @employees = Employee.all()
   erb(:division_edit)
 end
